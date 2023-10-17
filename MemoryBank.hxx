7f6472aef7a764c6003bbe8047f817edc03cb997
quazlee <quazlee29@gmail.com>
2023-10-17 11:29:34 -0400

    added members and methods

--- MemoryBank.hxx
+++ MemoryBank.hxx
@@ -1,8 +1,14 @@
+#include <vector>;
+
 class MemoryBank
 {
 private:
     /* data */
+    int size;
+    std::vector<two_byte> data(size);
 public:
     MemoryBank(/* args */);
     ~MemoryBank();
+    two_byte getData(int location);
+    void setData(int location, two_byte value);
 };
