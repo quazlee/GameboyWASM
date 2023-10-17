d03ac5aeca2241aff83bb16b6ee271ae55eff34f
quazlee <quazlee29@gmail.com>
2023-10-17 10:38:30 -0400

    Added member variables

--- Cpu.hxx
+++ Cpu.hxx
@@ -1,7 +1,20 @@
+#include <RegisterCollection.hxx>
+#include <Types.hxx>
+
 class Cpu
 {
 private:
-    /* data */
+    RegisterCollection *registers = new RegisterCollection();
+    Memory *memory = nullptr;
+    Ppu *ppu = nullptr;
+
+    four_byte programCounter = 0x0100;
+    four_byte stackPointer = 0xFFFE;
+
+    four_byte sysClock = 0xAB00;
+    bool halt = false;
+    bool ime = 0;
+
 public:
     Cpu(/* args */);
     ~Cpu();
