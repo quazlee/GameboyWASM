51ad917b9fb2fc8ad16465fb0a41f027a8d45354
quazlee <quazlee29@gmail.com>
2023-10-17 11:52:33 -0400

    implement interrupt

--- Cpu.cxx
+++ Cpu.cxx
@@ -21,3 +21,24 @@ unsigned_four_byte Cpu::fetch()
     programCounter++;
     return currentOpcode;
 }
+
+void Cpu::interrupt()
+{
+    if (ime && memory->readMemory(0xFFFF)) {
+            if (memory->readMemory(0xFFFF) & 1 && memory->readMemory(0xFF0F) & 1) {
+                interruptJump(0x40, 0);
+            }
+            if (memory->readMemory(0xFFFF) & 2 && memory->readMemory(0xFF0F) & 2) {
+                interruptJump(0x48, 1);
+            }
+            if (memory->readMemory(0xFFFF) & 4 && memory->readMemory(0xFF0F) & 4) {
+                interruptJump(0x50, 2);
+            }
+            if (memory->readMemory(0xFFFF) & 8 && memory->readMemory(0xFF0F) & 8) {
+                interruptJump(0x58, 3);
+            }
+            if (memory->readMemory(0xFFFF) & 16 && memory->readMemory(0xFF0F) & 16) {
+                interruptJump(0x60, 4);
+            }
+        }
+}
