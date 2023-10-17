fc6273c2a8228564c60c0852c79dd6fc4c864630
quazlee <quazlee29@gmail.com>
2023-10-17 12:00:57 -0400

    updated interrupt. implemented interruptJump

--- Cpu.cxx
+++ Cpu.cxx
@@ -24,21 +24,38 @@ unsigned_four_byte Cpu::fetch()
 
 void Cpu::interrupt()
 {
-    if (ime && memory->readMemory(0xFFFF)) {
-            if (memory->readMemory(0xFFFF) & 1 && memory->readMemory(0xFF0F) & 1) {
-                interruptJump(0x40, 0);
-            }
-            if (memory->readMemory(0xFFFF) & 2 && memory->readMemory(0xFF0F) & 2) {
-                interruptJump(0x48, 1);
-            }
-            if (memory->readMemory(0xFFFF) & 4 && memory->readMemory(0xFF0F) & 4) {
-                interruptJump(0x50, 2);
-            }
-            if (memory->readMemory(0xFFFF) & 8 && memory->readMemory(0xFF0F) & 8) {
-                interruptJump(0x58, 3);
-            }
-            if (memory->readMemory(0xFFFF) & 16 && memory->readMemory(0xFF0F) & 16) {
-                interruptJump(0x60, 4);
-            }
+    if (ime && memory->readMemory(0xFFFF))
+    {
+        if (memory->readMemory(0xFFFF) & 1 && memory->readMemory(0xFF0F) & 1)
+        {
+            interruptJump(0x40, 0);
         }
+        if (memory->readMemory(0xFFFF) & 2 && memory->readMemory(0xFF0F) & 2)
+        {
+            interruptJump(0x48, 1);
+        }
+        if (memory->readMemory(0xFFFF) & 4 && memory->readMemory(0xFF0F) & 4)
+        {
+            interruptJump(0x50, 2);
+        }
+        if (memory->readMemory(0xFFFF) & 8 && memory->readMemory(0xFF0F) & 8)
+        {
+            interruptJump(0x58, 3);
+        }
+        if (memory->readMemory(0xFFFF) & 16 && memory->readMemory(0xFF0F) & 16)
+        {
+            interruptJump(0x60, 4);
+        }
+    }
+}
+
+void Cpu::interruptJump(unsigned_four_byte location, int flag)
+{
+    stackPointer = registers.differenceDouble(stackPointer, 1);
+    memory->writeMemory(stackPointer, (programCounter >> 8));
+    stackPointer = registers.differenceDouble(stackPointer, 1);
+    memory->writeMemory(stackPointer, (programCounter & 0xff));
+    programCounter = location;
+    memory->writeMemory(0xFF0F, memory->readMemory(0xFF0F) & ~(1 << flag));
+    tickClock(20);
 }
