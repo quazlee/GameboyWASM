2be95144580f7b23dc6eb7921f27ac80981f02b8
quazlee <quazlee29@gmail.com>
2023-10-17 10:31:21 -0400

    Initial

---
+++ Types.hxx
@@ -0,0 +1,17 @@
+typedef short four_byte;
+typedef unsigned short unsigned_four_byte;
+typedef char two_byte;
+typedef unsigned char unsigned_two_byte;
+
+enum registerID
+{
+    B,
+    C,
+    D,
+    E,
+    H,
+    L,
+    HL,
+    A,
+    F
+};
