622e55c749f54d312e69dd2545eff32130978d23
quazlee <quazlee29@gmail.com>
2023-10-18 16:08:47 -0400

    added empty definitions

--- Gameboy.cxx
+++ Gameboy.cxx
@@ -1,7 +1,19 @@
 #include "Gameboy.hxx"
 
+Gameboy::Gameboy()
+{
+}
+
+Gameboy::~Gameboy()
+{
+}
+
 void Gameboy::initialize()
 {
     cpu->setMemory(memory);
     cpu->setPpu(ppu);
+}
+
+void Gameboy::mainLoop()
+{
 }
