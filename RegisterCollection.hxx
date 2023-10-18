f1750a1df34adca032fc994ec706d0f5862a3719
quazlee <quazlee29@gmail.com>
2023-10-18 16:55:49 -0400

    added emscripten

--- RegisterCollection.hxx
+++ RegisterCollection.hxx
@@ -1,12 +1,26 @@
 #ifndef REGISTERCOLLECTION_H
 #define REGISTERCOLLECTION_H
 
+#include "Types.hxx"
+
+#include <emscripten/emscripten.h>
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
 class RegisterCollection
 {
 private:
     /* data */
 public:
     RegisterCollection(/* args */);
-    ~RegisterCollection();
+    ~RegisterCollection();  
 };
+
+EMSCRIPTEN_BINDINGS(registercollection)
+{
+    class_<RegisterCollection>("RegisterCollection")
+    .constructor()
+    ;
+}
 #endif
