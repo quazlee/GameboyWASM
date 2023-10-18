381be0a2ea3058394577caa822f8a976b83581b3
quazlee <quazlee29@gmail.com>
2023-10-18 17:02:28 -0400

    added definitions

---
+++ Memory.cxx
@@ -0,0 +1,18 @@
+#include "Memory.hxx"
+
+Memory::Memory()
+{
+}
+
+Memory::~Memory()
+{
+}
+
+unsigned_two_byte Memory::readMemory(unsigned_four_byte address)
+{
+    return unsigned_two_byte();
+}
+
+void Memory::writeMemory(unsigned_four_byte address, unsigned_two_byte value)
+{
+}
