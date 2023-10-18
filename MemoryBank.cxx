1f5f5c2e8615a130fb74bc460db59504aee1d974
quazlee <quazlee29@gmail.com>
2023-10-17 23:13:51 -0400

    too lazy to make real message

--- MemoryBank.cxx
+++ MemoryBank.cxx
@@ -1,11 +1,16 @@
 #include "MemoryBank.hxx"
 
+MemoryBank::MemoryBank()
+{
+    data = new std::vector<unsigned_two_byte>(size);
+}
+
 unsigned_two_byte MemoryBank::getData(int location)
 {
-    return data[location];
+    return data->at(location);
 }
 
 void MemoryBank::setData(int location, unsigned_two_byte value)
 {
-    data[location] = value;
+    data->at(location) = value;
 }
--- MemoryBank.hxx
+++ MemoryBank.hxx
@@ -1,14 +1,15 @@
 #include <vector>
+#include "Types.hxx"
 
 class MemoryBank
 {
 private:
     /* data */
     int size;
-    std::vector<two_byte> data(size);
+    std::vector<unsigned_two_byte>* data;
 public:
     MemoryBank(/* args */);
     ~MemoryBank();
-    two_byte getData(int location);
-    void setData(int location, two_byte value);
+    unsigned_two_byte getData(int location);
+    void setData(int location, unsigned_two_byte value);
 };
