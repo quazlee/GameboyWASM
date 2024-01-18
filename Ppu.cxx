3e86b0e4fce2319cbe607555b0bb1aeace6e83ca
quazlee <quazlee29@gmail.com>
2024-01-18 18:43:53 -0500

    a

--- Ppu.cxx
+++ Ppu.cxx
@@ -20,20 +20,46 @@ void Ppu::setIsFrameReady(bool isReady)
 
 void Ppu::cycle()
 {
-    // if (mode == 2)
-    // {
-    //     modeTwo();
-    // }
-    // else if (mode == 3)
-    // {
-    //     modeThree();
-    // }
-    // else if (mode == 0)
-    // {
-    //     modeZero();
-    // }
-    // else if (mode == 1)
-    // {
-    //     modeOne();
-    // }
+    if (mode == 2)
+    {
+        modeTwo();
+    }
+    else if (mode == 3)
+    {
+        modeThree();
+    }
+    else if (mode == 0)
+    {
+        modeZero();
+    }
+    else if (mode == 1)
+    {
+        modeOne();
+    }
+}
+
+void Ppu::modeOne()
+{
+}
+
+void Ppu::modeTwo()
+{
+}
+
+void Ppu::modeThree()
+{
+}
+
+void Ppu::modeFour()
+{
+}
+
+int Ppu::getTileMapIndex(int *tilemap, int x, int y)
+{
+    return tilemap[x + (y * 256)];
+}
+
+void Ppu::setTileMapIndex(int *tilemap, int x, int y, int value)
+{
+    tilemap[x + (y * 256)] = value;
 }
--- Ppu.hxx
+++ Ppu.hxx
@@ -3,8 +3,12 @@
 class Ppu
 {
 private:
-    //Determines if the next frame is ready to be drawn. Will be true after scanline 153 is complete.
+    // Determines if the next frame is ready to be drawn. Will be true after scanline 153 is complete.
     bool isFrameReady = false;
+    int mode;
+
+    int tileMapOne = [65536];
+    int tileMapTwo = [65536];
 
 public:
     Ppu(/* args */);
@@ -13,6 +17,14 @@ public:
     bool getIsFrameReady();
     void setIsFrameReady(bool isReady);
 
-    //General function that drives the ppu. One pass of each mode lasts two clock cycles.
+    // General function that drives the ppu. One pass of each mode lasts two clock cycles.
     void cycle();
+
+    void modeOne();
+    void modeTwo();
+    void modeThree();
+    void modeFour();
+
+    int getTileMapIndex(int *tilemap,int x, int y);
+    void setTileMapIndex(int *tilemap, int x, int y, int value);
 };
